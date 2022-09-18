5488ad675fd1cabfd0d7bcf03fadfe0ca9faa23e
Private User <1854985+inflatador@users.noreply.github.com>
2022-09-18 16:50:32 -0500

    working but needs better validation for releases master releases

--- jukeboxen.py
+++ jukeboxen.py
@@ -5,15 +5,15 @@
 # Now you can name servers/containers/whatever after your favorite band's
 # songs!
 
-# Version: 0.0.1a
+# Version: 0.0.2a
 # Author: Brian King
 # License: Apache 2.0
 
 import discogs_client
-# import keychain
 import logging
 import os
 import plac
+import validators
 
 jb_logger = logging.getLogger("jukeboxen")
 logging.basicConfig(level=logging.DEBUG)
@@ -36,7 +36,7 @@ def get_artist_releases(jb_artist_id, jb_client):
     jb_logger.debug("Found the following release IDs: {}".format(jb_releases))
     return jb_releases
 
-def get_all_tracks(jb_releases, jb_client):
+def get_song_names(jb_releases, jb_client):
 # using "MasterRelease" instead of "Release" as it seems to have the correct
 # data, ref
 # https://python3-discogs-client.readthedocs.io/en/latest/fetching_data_repl.html#masterrelease
@@ -47,6 +47,29 @@ def get_all_tracks(jb_releases, jb_client):
             song_names.append(track.title)
 
     jb_logger.debug("I got the tracks as {}".format(song_names))
+    return song_names
+
+# FIXME: Better transformation
+def convert_to_hostnames(song_names):
+    converted_hostnames = []
+    for song_name in song_names:
+        # song_name = song_name.replace(".", "")
+        song_name = song_name.replace(" ", "-")
+        song_name = song_name.replace("--", "-")
+        song_name = song_name.replace("'", "")
+        song_name = song_name.lower()
+        converted_hostnames.append(song_name)
+    return converted_hostnames
+
+# FIXME: Better validation
+def validate_hostnames(converted_hostnames):
+    validated_hostnames = []
+    for hostname in converted_hostnames:
+        jb_logger.debug("Validating hostname {}...".format(hostname))
+        if validators.domain("{}.example.com".format(hostname)):
+            validated_hostnames.append(hostname)
+    return validated_hostnames
+
 
 @plac.annotations(
     # operation = plac.Annotation("operation to perform: bulk, create, delete, search, stats, update",
@@ -62,14 +85,16 @@ def main(artist_name):
     jb_logger.debug("Artist name received: {}".format(artist_name))
     # discogs requires a unique user-agent per application
     jb_id = "Jukeboxen"
-    jb_vers = "0.0.1a"
+    jb_vers = "0.0.2a"
     jb_user_agent = "{}/{}".format(jb_id, jb_vers)
     jb_token = os.environ["JB_TOKEN"]
     jb_client = discogs_client.Client(jb_user_agent, user_token=jb_token)
     jb_artist_id = get_artist_id(artist_name, jb_client)
     jb_releases = get_artist_releases(jb_artist_id, jb_client)
-    get_all_tracks(jb_releases, jb_client)
-
+    song_names = get_song_names(jb_releases, jb_client)
+    converted_hostnames = convert_to_hostnames(song_names)
+    validated_hostnames = validate_hostnames(converted_hostnames)
+    print ("Validated hostnames: {}".format(validated_hostnames))
 if __name__ == '__main__':
     import plac
     plac.call(main)
